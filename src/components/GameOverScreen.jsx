0156670dedebe1fcb46dc4683a9a1349c76b7e1d
Kostas Priftis <priftis.kostas1997@gmail.com>
2024-09-08 21:01:20 +0300

    Remove console logs

--- src/components/GameOverScreen.jsx
+++ src/components/GameOverScreen.jsx
@@ -22,7 +22,9 @@ export default function GameOver({
   if (showContent) {
     return (
       <div className="fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center flex-col z-50">
-        <h1 className="text-white text-[6rem] font-serif font-bold">GAME OVER</h1>
+        <h1 className="text-white text-[6rem] font-serif font-bold">
+          GAME OVER
+        </h1>
         <h1
           className={`${
             playerWon === true ? "text-green-400" : "text-red-400"
@@ -32,9 +34,11 @@ export default function GameOver({
         </h1>
         <div className="flex flex-row items-center gap-2">
           <span className="text-white text-xl">Word was</span>
-            <h2 className="text-white text-2xl underline p-2 font-serif">{result}</h2>
+          <h2 className="text-white text-2xl underline p-2 font-serif uppercase">
+            {result}
+          </h2>
         </div>
-      
+
         <button
           onClick={() => setRestartGame(true)}
           className="bg-slate-500 p-0 rounded-md text-xl w-32 mt-12 h-14 font-serif text-white font-bold hover:bg-slate-800"
@@ -52,4 +56,5 @@ GameOver.propTypes = {
   visible: PropTypes.bool,
   setRestartGame: PropTypes.func,
   playerWon: PropTypes.bool,
+  result: PropTypes.string,
 };
--- src/components/GuessesMade.jsx
+++ src/components/GuessesMade.jsx
@@ -1,25 +1,33 @@
+import PropTypes from 'prop-types'
 export default function GuessesMade({ lettersGuessed, wordToGuess }) {
   const lettersInWord =
     wordToGuess && wordToGuess.length > 0 && wordToGuess.split("");
-  return (
-    <div className="flex flex-row items-center gap-2">
-      <span className="text-2xl text-white">Guesses made:</span>
-      <div className="flex flex-row items-center gap-1">
-        {lettersGuessed.map((letter, idx) => {
-          const isCorrect = lettersInWord.includes(letter.toLowerCase());
-          console.log(letter, isCorrect);
-          return (
-            <span
-              key={idx}
-              className={` font-bold uppercase text-2xl ${
-                isCorrect === true ? "text-green-400" : "text-red-500"
-              }`}
-            >
-              {letter},
-            </span>
-          );
-        })}
+  if (lettersGuessed.length > 0) {
+    return (
+      <div className="flex flex-row items-center gap-2">
+        <span className="text-2xl text-white">Guesses made:</span>
+        <div className="flex flex-row items-center gap-1">
+          {lettersGuessed.map((letter, idx) => {
+            const isCorrect = lettersInWord.includes(letter.toLowerCase());
+            return (
+              <span
+                key={idx}
+                className={` font-bold uppercase text-2xl ${
+                  isCorrect === true ? "text-green-400" : "text-red-500"
+                }`}
+              >
+                {letter},
+              </span>
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+
+GuessesMade.propTypes = {
+    lettersGuessed: PropTypes.array,
+    wordToGuess: PropTypes.string
 }
